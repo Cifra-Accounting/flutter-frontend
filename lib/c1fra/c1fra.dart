4da3eb673f02df8ad16b2f63a0138b51eec067b7
Asbecov <m2311505@edu.misis.ru>
2024-10-25 18:54:45 +0300

    feature: created pre-commit hooks

---
+++ lib/c1fra/c1fra.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class C1fra extends StatelessWidget {
+  const C1fra({super.key});
+
+  @override
+  Widget build(BuildContext context) => const MaterialApp();
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:cifra_app/c1fra/c1fra.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const C1fra());
+}
